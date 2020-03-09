e7bea5e00dd7fdfbeb3cdb816b93f073aa2a944f
myyong <myong@turing.ac.uk>
2020-03-09 12:54:24 +0000

    Installation of clarity ui

--- infrastructure/toymodel0/client/src/app/app.module.ts
+++ infrastructure/toymodel0/client/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// import { ClarityModule } from "@clr/angular";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientComponent } from './perspectives/patient/patient.component';
+import { ClinicianComponent } from './perspectives/clinician/clinician.component';
+import { EngineerComponent } from './perspectives/engineer/engineer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientComponent,
+    ClinicianComponent,
+    EngineerComponent
   ],
   imports: [
     BrowserModule,
