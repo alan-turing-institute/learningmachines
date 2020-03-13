9a18285e83538c756f452fa0f4b0e610a0945fd9
Yong May Yee <myong@turing.ac.uk>
2020-03-13 18:21:51 +0000

    Fixed scatterplot, need to switch to multiline

--- infrastructure/toymodel0/client/src/app/vis/relapse/relapse.component.ts
+++ infrastructure/toymodel0/client/src/app/vis/relapse/relapse.component.ts
@@ -14,37 +14,58 @@ export class RelapseComponent implements OnInit {
 
   createChart() {
     
-
-    
     this.chart = new Chart('relapse', {
       type: 'scatter',
       data: {
-        labels: ['3 months', '6 months', '9 months'],
-        datasets:[{
-          label: "Relapse",
-          data: [1, 2, 3]
-        }]
+          datasets: [{
+              label: 'Relapse',
+              data: [null, 50, null, null],
+              spanGaps: true
+          }]
       },
       options: {
-        scales: {
-          yAxes:[{
-            ticks: {
-              display:false
-            },
-            scaleLabel: {
-              display: true,
-              labelString: "Occurence"
-            }}
-          ]
-        },
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'time'
+          scales: {
+              xAxes: [{
+                  type: 'category',
+                  position: 'bottom',
+                  labels: ['Baseline', '3 months', '6 months', '9 months'],
+              }]
           }
-        }],
       }
-    });
+  });
+    
+    // this.chart = new Chart('relapse', {
+    //   type: 'scatter',
+    //   data: {
+    //     labels: ['Baseline', '3 months', '6 months', '9 months'],
+    //     datasets:[{
+    //       label: "Relapse",
+    //       data: [{x: '3 months', y: 3},
+    //         {x: '6 months', y: 3},
+    //         {x: '9 months', y: 5}
+    //       ]
+    //     }]
+    //   },
+    //   options: {
+    //     scales: {
+    //       yAxes:[{
+    //         ticks: {
+    //           display:false
+    //         },
+    //         scaleLabel: {
+    //           display: true,
+    //           labelString: "Occurence"
+    //         }}
+    //       ]
+    //     },
+    //     xAxes: [{
+    //       type: {
+    //         display: true,
+    //         labelString: 'time'
+    //       }
+    //     }],
+    //   }
+    // });
   }
 
   ngAfterViewInit():void {
