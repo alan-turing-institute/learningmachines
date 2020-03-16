b7d78018f170c3fa05716356b4842668093f0564
Gordon Yip <zcapkhy@ucl.ac.uk>
2020-03-16 18:53:02 +0000

    database structure

---
+++ MS_example/fill_database.py
@@ -0,0 +1,73 @@
+from flask import Flask
+from database import db
+from tables import DM,CE,QS
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    with app.app_context():
+        db.init_app(app)
+    return app
+
+
+def setup_database(app, datapath, POPULATE_DB=True):
+    if POPULATE_DB:
+        print("Populating database...")
+        with app.app_context():
+            db.drop_all()
+            print("Dropped all previous columns")
+            db.create_all()
+            print("Created new empty database")
+            import pandas as pd
+            dm_file = pd.read_csv(f"{datapath}/dm.csv")
+            ce_file = pd.read_csv(f"{datapath}/ce.csv")
+            qs_file = pd.read_csv(f"{datapath}/qs.csv")
+            print("filling Patients Demography(DM)")
+            for row in range(len(dm_file)):
+                DM.populate(patient_id=dm_file.iloc[row].USUBJID,
+                            domain=dm_file.iloc[row].DOMAIN,
+                            age=dm_file.iloc[row].AGE,
+                            gender=dm_file.iloc[row].SEX,
+                            race=dm_file.iloc[row].RACE,
+                            country=dm_file.iloc[row].COUNTRY)
+            print("filled DM table")
+            print("filling Clinical Event(CE)")
+            for row in range(len(ce_file)):
+                CE.add_clinical_record(
+                    patient_id=ce_file.iloc[row].USUBJID,
+                    domain=ce_file.iloc[row].DOMAIN,
+                    reported_term=ce_file.iloc[row].CETERM,
+                    modify_term=ce_file.iloc[row].CEMODIFY,
+                    dict_term=ce_file.iloc[row].CEDECOD,
+                    category=ce_file.iloc[row].CECAT,
+                    body_system=ce_file.iloc[row].CEBODSYS,
+                    severity=ce_file.iloc[row].CESEV,
+                    serious_event=ce_file.iloc[row].CESER,
+                    start_day=ce_file.iloc[row].CESTDY,
+                    end_day=ce_file.iloc[row].CEENDY,
+                    day=ce_file.iloc[row].CEDY, )
+            print("filled CE table")
+            print("filling Quetionaire(QS)")
+            for row in range(len(qs_file)):
+                one_event = QS(
+                    patient_id=qs_file.iloc[row].USUBJID,
+                    domain=qs_file.iloc[row].DOMAIN,
+                    test_code=qs_file.iloc[row].QSTESTCD,
+                    test_dption=qs_file.iloc[row].QSTEST,
+                    test_category=qs_file.iloc[row].QSCAT,
+                    test_score_raw=qs_file.iloc[row].QSORRES,
+                    test_score_num=qs_file.iloc[row].QSSTRESN,
+                    test_day=qs_file.iloc[row].QSDY,
+                    visit=qs_file.iloc[row].VISIT,
+                    visit_day=qs_file.iloc[row].VISITDY,
+                )
+                db.session.add(one_event)
+                db.session.flush()
+            db.session.commit()
+            print("filled QS table")
+    else:
+        print("database setup passed.")
+    print("database setup and ready to use.")
+
+
---
+++ MS_example/tables.py
@@ -0,0 +1,110 @@
+from database import db
+
+class DM(db.Model):
+    __tablename__ = 'patient_bkg'
+    patient_id = db.Column(db.String(), unique=True, nullable=False, primary_key=True)
+    domain = db.Column(db.String(), nullable=True)
+    age = db.Column(db.Integer, nullable=True)
+    gender = db.Column(db.String(), nullable=True)
+    race = db.Column(db.String(), nullable=True)
+    country = db.Column(db.String(), nullable=True)
+    clinical_event = db.relationship('CE', backref='patient', lazy=True)
+    questionaire = db.relationship('QS', backref='patient', lazy=True)
+
+    def __init__(self, patient_id, domain, age, gender, race, country):
+        self.patient_id = patient_id
+        self.domain = domain
+        self.age = age
+        self.gender = gender
+        self.race = race
+        self.country = country
+
+    def __repr__(self):
+        return f"DM Table('ID: {self.patient_id}',Age:'{self.age}','Domain:{self.domain}','Sex:{self.gender}')"
+
+    @classmethod
+    def populate(self, patient_id, domain, age, gender, race, country):
+        person = self(patient_id, domain, age, gender, race, country)
+        db.session.add(person)
+        db.session.commit()
+
+
+class CE(db.Model):
+    __tablename__ = 'clinical_event'
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    patient_id = db.Column(db.String(120), db.ForeignKey('patient_bkg.patient_id'), nullable=False)
+    domain = db.Column(db.String(2), nullable=True)
+    reported_term = db.Column(db.String(100), nullable=True)
+    modify_term = db.Column(db.String(100), nullable=True)
+    dict_term = db.Column(db.String(100), nullable=True)
+    category = db.Column(db.String(100), nullable=True)
+    body_system = db.Column(db.String(100), nullable=True)
+    severity = db.Column(db.String(100), nullable=True)
+    serious_event = db.Column(db.String(1), nullable=True)
+    start_day = db.Column(db.Integer, nullable=True)
+    end_day = db.Column(db.Integer, nullable=True)
+    day = db.Column(db.Integer, nullable=True)
+
+    def __init__(self, patient_id, domain, reported_term, modify_term, dict_term, category, body_system, severity,
+                 serious_event, start_day, end_day, day):
+        self.patient_id = patient_id
+        self.domain = domain
+        self.reported_term = reported_term
+        self.modify_term = modify_term
+        self.dict_term = dict_term
+        self.category = category
+        self.body_system = body_system
+        self.severity = severity
+        self.start_day = start_day
+        self.day = day
+        self.end_day = end_day
+
+    def __repr__(self):
+        return f"Clinical Event('ID: {self.id}',Patient ID:'{self.patient_id}','Incident:{self.reported_term}','Day:{self.day}','Start Day:{self.start_day}')"
+
+    @classmethod
+    def add_clinical_record(self, patient_id, domain, reported_term, modify_term, dict_term, category, body_system,
+                            severity, serious_event, start_day, end_day, day):
+        CE_record = self(patient_id, domain, reported_term, modify_term, dict_term, category, body_system, severity,
+                         serious_event, start_day, end_day, day)
+        db.session.add(CE_record)
+        db.session.commit()
+
+
+class QS(db.Model):
+    __tablename__ = 'questionaire'
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    patient_id = db.Column(db.String(), db.ForeignKey('patient_bkg.patient_id'), nullable=False)
+    domain = db.Column(db.String(2), nullable=True)
+    test_code = db.Column(db.String(50), nullable=True)
+    test_dption = db.Column(db.String(100), nullable=True)
+    test_category = db.Column(db.String(30), nullable=True)
+    test_score_raw = db.Column(db.String(100), nullable=True)
+    test_score_num = db.Column(db.Float, nullable=True)
+    test_day = db.Column(db.Integer, nullable=True)
+    visit = db.Column(db.String(50), nullable=True)
+    visit_day = db.Column(db.Integer, nullable=True)
+
+    def __init__(self, patient_id, domain, test_code, test_dption, test_category, test_score_raw, test_score_num,
+                 test_day, visit, visit_day):
+        self.patient_id = patient_id
+        self.domain = domain
+        self.test_code = test_code
+        self.test_dption = test_dption
+        self.test_category = test_category
+        self.test_score_raw = test_score_raw
+        self.test_score_num = test_score_num
+        self.test_day = test_day
+        self.visit = visit
+        self.visit_day = visit_day
+
+    def __repr__(self):
+        return f"Questionaire('ID: {self.id}',Patient ID:'{self.patient_id}','Test Performed:{self.test_code}','Category:{self.test_category}','Score:{self.test_score_raw}','test day:{self.test_day})"
+
+    @classmethod
+    def add_questionaire(self, patient_id, domain, test_code, test_dption, test_category, test_score_raw,
+                         test_score_num, test_day, visit, visit_day):
+        QS_record = self(patient_id, domain, test_code, test_dption, test_category, test_score_raw, test_score_num,
+                         test_day, visit, visit_day)
+        db.session.add(QS_record)
+        db.session.commit()
