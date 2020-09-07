39fb56574af38bf783e7cb74a956e93d7fbe34fa
Yong May Yee <myong@turing.ac.uk>
2020-09-07 17:53:46 +0100

    Test dir structure for tm1

---
+++ infrastructure/ui/tm1/client/src/app/perspectives/clinician/clinician.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clinician',
+  templateUrl: './clinician.component.html',
+  styleUrls: ['./clinician.component.css']
+})
+export class ClinicianComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
